d2700903c25a8c4e5ea0386954b089d0cb4156ac
Nitin Kumar <Nitin@59.161.181.74.static-Hyderabad.vsnl.net.in>
2016-02-27 18:18:12 +0530

    tooltip anywhere module changes committed.

--- js/tooltip_anywhere.js
+++ js/tooltip_anywhere.js
@@ -1,17 +1,37 @@
 (function ($) {
-  Drupal.behaviors.tooltipster = {
+  Drupal.behaviors.tooltip_anywhere = {
     attach:function (context, settings) {
       if (Drupal.settings.vars) {
+        var type_tooltip = '';
         var sel = Drupal.settings.vars.selector.length;
         var inputObj = '';
         for(var i = 0; i < sel; i++) {
-          inputObj = {
-            content: Drupal.settings.vars.content[i],
-            position: Drupal.settings.vars.position[i],
-            theme: Drupal.settings.vars.properties[i].theme,
-          };
-          //Object.assign(inputObj, Drupal.settings.vars.properties[i]);
-          $(Drupal.settings.vars.selector[i]).tooltipster(inputObj);
+
+          var processed = false;
+          type_tooltip = Drupal.settings.vars.type[i];
+          switch (type_tooltip) {
+            case 'id' :
+            case 'class' :
+              if ($(Drupal.settings.vars.selector[i]).hasClass("tooltipstered")) {
+                processed = true;
+              }
+              break;
+            default :
+              if ($(Drupal.settings.vars.selector[i]).hasClass("tooltipstered")) {
+                processed = true;
+              }
+              break;
+          }
+
+          if (!processed) {
+            inputObj = {
+              content: Drupal.settings.vars.content[i],
+              position: Drupal.settings.vars.position[i],
+              theme: Drupal.settings.vars.properties[i].theme,
+            };
+            $(Drupal.settings.vars.selector[i]).tooltipster(inputObj);
+          }
+
         }
 
       }
